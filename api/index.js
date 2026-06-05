32e73c12d78b34efdbe8136708c177f37b3b020a
NeuroPyrox <NeuroPyrox@Gmail.com>
2026-06-04 19:10:04 -0700

    Switch to custom server.js and configure routing for Vercel

--- api/index.js
+++
@@ -1,8 +0,0 @@
-// This is a serverless function that Vercel will run.
-// It receives a request (req) and returns a response (res).
-module.exports = (req, res) => {
-  res.status(200).json({
-    message: "Hello World from Node.js on Vercel!",
-    timestamp: new Date().toISOString()
-  });
-};
